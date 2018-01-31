a9d2293dfa745392c117f6416da12089c7c7c288
Jeff Campbell <jeff.campbell@crown.com>
2018-01-31 14:33:25 -0500

    Added timer files under HAL

---
+++ src/hal/AT4313/timer.c
@@ -0,0 +1,37 @@
+/**
+ *	timer.c
+ *
+ *	Used in timer logic.  This is the implementation
+ *	for the AT4313 chip.
+ *
+ *	Jeff Campbell
+ *	1/31/18
+ *
+ */
+
+/* Includes */ 
+#include "typedefs.h"
+#include "timer.h"
+
+/* Prototypes */
+void gvTimer_sleepMs(uint16_t uwTimeMs);
+
+/* Local Variables */
+
+/**
+ *	void gvTimer_sleepMs(uint8_t ubTimeMs)
+ *
+ *	Description:
+ *		Sleeps for the given amount of time.
+ *
+ *	Parameters:
+ *		ubTimeMs = Time to sleep, in ms.
+ *
+ *	Returns:
+ *		N/A
+ *
+ */
+void gvTimer_sleepMs(uint16_t uwTimeMs)
+{
+	// todo: implement this
+}
---
+++ src/hal/windows/timer.h
@@ -0,0 +1,21 @@
+/**
+ *	timer.h
+ *
+ *	Interface for timer.c.
+ *
+ *	Jeff Campbell
+ *	1/31/18
+ *
+ */
+
+#ifndef TIMER_H
+#define TIMER_H
+
+/* Includes */
+
+/* Defines */
+
+/* Global Functions */
+extern void gvTimer_sleepMs(uint16_t uwTimeMs);
+
+#endif /* #ifndef TIMER_H */
