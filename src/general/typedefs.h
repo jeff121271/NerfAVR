1ef7c9afaf6e3183b3b180f958787e1d04461e34
Jeff Campbell <jeff.campbell@crown.com>
2018-02-01 08:27:29 -0500

    Added volatile types to typedefs

--- src/general/typedefs.h
+++ src/general/typedefs.h
@@ -12,18 +12,38 @@
 #define TYPEDEFS_H
 
 /* Defines */
+
+/* Constant value for True */
 #define TRUE 1u
+
+/* Constant value for False */
 #define FALSE 0u
+
+/* Null value */
 #define NULL 0u
+
+/* Null value, but as a pointer */
 #define NULL_PTR ((void *)0u)
 
+/* Standard integer types */
 typedef long int32_t;
 typedef unsigned long uint32_t;
 typedef int int16_t;
 typedef unsigned int uint16_t;
 typedef char int8_t;
 typedef unsigned char uint8_t;
+
+/* Boolean */
 typedef unsigned char boolean;
+
+/* Volatile definitions */
+typedef volatile long vint32_t;
+typedef volatile unsigned long vuint32_t;
+typedef volatile int vint16_t;
+typedef volatile unsigned int vuint16_t;
+typedef volatile char vint8_t;
+typedef volatile unsigned char vuint8_t;
+
 // todo: Check later if this ever really gets used
 typedef union nibbles
 {
