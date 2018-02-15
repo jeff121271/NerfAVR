a538c727c169d9f3d46dcf580071b3c2dc6ae1f7
jeff121271 <jeff.campbell121271@gmail.com>
2018-02-15 18:46:09 -0500

    Created flywheel driver

---
+++ src/drivers/flywheel_driver.c
@@ -0,0 +1,23 @@
+/**
+ *  flywheel_driver.c
+ *
+ *  Driver for the flywheel system.
+ *
+ *  Jeff Campbell
+ *  2/15/18
+ *
+ */ 
+
+ /* Includes */
+ #include <stdint.h>
+ #include "flywheel_driver.h"
+
+ /* Prototypes */
+ void gvFlywheel_process(uint16_t uwCallRateMs);
+
+ /* Local Variables */
+
+ void gvFlywheel_process(uint16_t uwCallRateMs)
+ {
+    
+ }
---
+++ src/drivers/flywheel_driver.h
@@ -0,0 +1,21 @@
+/**
+ *  flywheel_driver.h
+ *
+ *  Interface file for the flywheel driver.
+ *
+ *  Jeff Campbell
+ *  2/15/18
+ *
+ */ 
+
+#ifndef FLYWHEEL_DRIVER_H
+#define FLYWHEEL_DRIVER_H
+
+/* Includes */
+
+/* Defines */
+
+/* Global Functions */
+extern void gvFlywheel_process(uint16_t uwCallRateMs);
+
+#endif /* #ifndef FLYWHEEL_DRIVER_H */
