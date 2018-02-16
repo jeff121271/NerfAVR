a1b82ee9b44ae3d6c5da7c55aa60f4f42a43f256
jeff121271 <jeff.campbell121271@gmail.com>
2018-02-15 19:00:12 -0500

    Created state machine for flywheel driver

--- src/drivers/flywheel_driver.c
+++ src/drivers/flywheel_driver.c
@@ -8,16 +8,115 @@
  *
  */ 
 
- /* Includes */
- #include <stdint.h>
- #include "flywheel_driver.h"
+/* Includes */
+#include <stdint.h>
+#include "pins.h"
+#include "pwm.h"
+#include "flywheel_driver.h"
 
- /* Prototypes */
- void gvFlywheel_process(uint16_t uwCallRateMs);
+/* Prototypes */
+void gvFlywheel_process(uint16_t uwCallRateMs);
 
- /* Local Variables */
+/* Local Variables */
 
- void gvFlywheel_process(uint16_t uwCallRateMs)
- {
-    
- }
+/* Desired flywheel speed (in terms of PWM command) */
+static uint8_t xubDesiredSpeed = 0u;
+
+/* Raw quadrature counts from flywheel #1 */
+static volatile uint8_t xubWheel1Counts = 0u;
+
+/* Raw quadrature counts from flywheel #2 */
+static volatile uint8_t xubWheel2Counts = 0u;
+
+/**
+ *  void gvFlywheel_process(uint16_t uwCallRateMs)
+ *
+ *  Description:
+ *      State machine for the flywheel driver.
+ *
+ *  Parameters:
+ *      uwCallRateMs = Call rate of function, in ms
+ *
+ *  Returns:
+ *      N/A
+ *
+ */
+void gvFlywheel_process(uint16_t uwCallRateMs)
+{
+    static flywheel_states_t seState = FLY_STATE_INIT;
+
+    /* Process state actions */
+    switch (seState)
+    {
+        case FLY_STATE_INIT:
+            /* Do nothing */
+            break;
+
+        case FLY_STATE_IDLE:
+            /* Make sure motors are turned off */
+            gvPWM_setCmd1(0u);
+            gvPWM_setCmd2(0u);
+            break;
+
+        case FLY_STATE_RAMP_UP:
+            /* Drive both motors with commanded speed */
+            gvPWM_setCmd1(xubDesiredSpeed);
+            gvPWM_setCmd2(xubDesiredSpeed);
+            break;
+
+        case FLY_STATE_MAINTAIN:
+            /* Set master command */
+            gvPWM_setCmd1(xubDesiredSpeed);
+
+            /* Increase slave command if it is slower */
+
+            /* Otherwise, decrement it */
+
+            break;
+
+        default:
+            /* Do nothing */
+            break;
+    }
+
+    /* Process state transitions */
+    switch (seState)
+    {
+        case FLY_STATE_INIT:
+            /* Go to idle state */
+            seState = FLY_STATE_IDLE;
+            break;
+
+        case FLY_STATE_IDLE:
+            /* If trigger is pressed, ramp up */
+            if ( PIN_LOGIC_HIGH == gubPins_read(PIN_TRIGGER) )
+            {
+                seState = FLY_STATE_RAMP_UP;
+            }
+            break;
+
+        case FLY_STATE_RAMP_UP:
+            /* If trigger is released, go to idle */
+            if ( PIN_LOGIC_LOW == gubPins_read(PIN_TRIGGER) )
+            {
+                seState = FLY_STATE_IDLE;
+            }
+            else
+            {
+                /* Otherwise, check if master is at speed */
+                // TODO: speed calculation
+            }
+            break;
+
+        case FLY_STATE_MAINTAIN:
+            /* Only exit when trigger is released */
+            if ( PIN_LOGIC_LOW == gubPins_read(PIN_TRIGGER) )
+            {
+                seState = FLY_STATE_IDLE;
+            }
+            break;
+
+        default:
+            break;
+    }
+}
--- src/drivers/flywheel_driver.h
+++ src/drivers/flywheel_driver.h
@@ -15,6 +15,15 @@
 
 /* Defines */
 
+/* Enumeration of flywheel driver states */
+typedef enum flywheel_states
+{
+    FLY_STATE_INIT = 0,     /* Init state */
+    FLY_STATE_IDLE,         /* Idle state - no command */
+    FLY_STATE_RAMP_UP,      /* Ramp up state - spin both motors up to speed */
+    FLY_STATE_MAINTAIN      /* Maintain state - try to match slave speed to master speed */
+} flywheel_states_t;
+
 /* Global Functions */
 extern void gvFlywheel_process(uint16_t uwCallRateMs);
 
